9d17fa7cba9b1c0d037eb6296ba14029f33cb10c
Ivica Batinic <ivica.batinic@infinum.hr>
2022-11-11 18:46:06 +0100

    Init generator tests

---
+++ packages/plop-next-ts-generators/tests/file-helper.js
@@ -0,0 +1,42 @@
+import fs from 'node:fs';
+import { resolve } from 'node:path';
+
+export const getFileHelper = () => {
+	let cleanupFiles = [];
+
+	const cleanup = () => {
+		if (!cleanupFiles.length) return;
+
+		for (const file of cleanupFiles) {
+			try {
+				fs.unlinkSync(file);
+			} catch (e) {
+				// ignore
+			}
+		}
+
+		cleanupFiles = [];
+	};
+
+	afterEach(() => {
+		cleanup();
+	});
+
+	const getFilePath = async (path) => {
+		const expectedFilePath = resolve(__dirname, path);
+
+		cleanupFiles.push(expectedFilePath);
+
+		try {
+			await fs.promises.unlink(expectedFilePath);
+		} catch (e) {
+			// ignore
+		}
+
+		return expectedFilePath;
+	};
+
+	return {
+		getFilePath,
+	};
+};
