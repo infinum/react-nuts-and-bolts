1f1bd638e1573a3bd0151d6659b1563793a2ab3c
Ivica Batinic <ivica.batinic@infinum.hr>
2022-11-22 10:59:35 +0100

    FInish theme generator tests

--- packages/plop-next-ts-generators/tests/theme.test.js
+++ packages/plop-next-ts-generators/tests/theme.test.js
@@ -3,7 +3,13 @@ import * as fs from 'node:fs';
 import { render, waitFor } from 'cli-testing-library';
 
 import { getFileHelper } from './file-helper.js';
-import { ADD_FOUNDATION_COMMAND, INIT_COMMAND } from '../constants.js';
+import {
+	ADD_COMPONENT_COMMAND,
+	ADD_FOUNDATION_COMMAND,
+	INIT_COMMAND,
+	MULTI_PART_COMPONENT,
+	SINGLE_PART_COMPONENT,
+} from '../constants.js';
 
 describe('theme generators', () => {
 	const { getFilePath } = getFileHelper();
@@ -41,7 +47,7 @@ describe('theme generators', () => {
 		const themeFilePath = await getFilePath(`./output/src/styles/theme/index.ts`);
 		const colorsFoundationFilePath = await getFilePath(`./output/src/styles/theme/foundations/colors.ts`);
 
-		const { findByText, userEvent, getStdallStr } = cli;
+		const { findByText, userEvent } = cli;
 
 		// Init theme file first
 		const themeInitializer = await render('plop', [
@@ -70,22 +76,149 @@ describe('theme generators', () => {
 
 		expect(await findByText('Select foundation:')).toBeInTheConsole();
 
-		userEvent.keyboard('[ArrowDown]');
-		userEvent.keyboard('[ArrowDown]');
-		userEvent.keyboard('[ArrowDown]');
+		await userEvent.keyboard('[ArrowDown]');
+		await userEvent.keyboard('[ArrowDown]');
+		await userEvent.keyboard('[ArrowDown]');
 
 		expect(await findByText(`❯ colors`)).toBeInTheConsole();
 
-		await userEvent.keyboard('[Enter]');
+		userEvent.keyboard('[Enter]');
 
 		await waitFor(async () => fs.promises.stat(colorsFoundationFilePath));
 
-		console.log(getStdallStr());
-
 		const colorFoundation = fs.readFileSync(colorsFoundationFilePath, 'utf8');
-		const theme = fs.readFileSync(themeFilePath, 'utf8');
 
 		expect(colorFoundation).toMatchSnapshot();
+
+		await waitFor(async () => {
+			const theme = fs.readFileSync(themeFilePath, 'utf8');
+
+			expect(theme).toContain(`import { colors } from './foundations/colors';`);
+			expect(theme).toContain(`colors,`);
+		});
+
+		const theme = fs.readFileSync(themeFilePath, 'utf8');
+		expect(theme).toMatchSnapshot();
+	});
+
+	it('should add single-part button component theme', async () => {
+		const themeFilePath = await getFilePath(`./output/src/styles/theme/index.ts`);
+		const buttonComponentThemeFilePath = await getFilePath(`./output/src/styles/theme/components/button.ts`);
+
+		const { findByText, userEvent } = cli;
+
+		// Init theme file first
+		const themeInitializer = await render('plop', [
+			`--plopfile=${resolve(__dirname, './plopfile.js')}`,
+			`--dest=${resolve(__dirname, './output')}`,
+			'theme',
+		]);
+
+		expect(await themeInitializer.findByText('Select a command:')).toBeInTheConsole();
+
+		expect(await themeInitializer.findByText(`❯ ${INIT_COMMAND}`)).toBeInTheConsole();
+
+		themeInitializer.userEvent.keyboard('[Enter]');
+
+		await waitFor(async () => fs.promises.stat(themeFilePath));
+
+		// Add colors foundation
+
+		expect(await findByText('Select a command:')).toBeInTheConsole();
+
+		userEvent.keyboard('[ArrowDown]');
+		userEvent.keyboard('[ArrowDown]');
+
+		expect(await findByText(`❯ ${ADD_COMPONENT_COMMAND}`)).toBeInTheConsole();
+
+		userEvent.keyboard('[Enter]');
+
+		expect(await findByText('Enter component name:')).toBeInTheConsole();
+
+		await userEvent.keyboard('Button');
+		await userEvent.keyboard('[Enter]');
+
+		expect(await findByText('Select a component type:')).toBeInTheConsole();
+		expect(await findByText(`❯ ${SINGLE_PART_COMPONENT}`)).toBeInTheConsole();
+
+		await userEvent.keyboard('[Enter]');
+
+		await waitFor(async () => fs.promises.stat(buttonComponentThemeFilePath));
+
+		const buttonComponentTheme = fs.readFileSync(buttonComponentThemeFilePath, 'utf8');
+
+		expect(buttonComponentTheme).toMatchSnapshot();
+
+		await waitFor(async () => {
+			const theme = fs.readFileSync(themeFilePath, 'utf8');
+
+			expect(theme).toContain(`import { buttonTheme as Button } from './components/button';`);
+			expect(theme).toContain(`Button,`);
+		});
+
+		const theme = fs.readFileSync(themeFilePath, 'utf8');
+		expect(theme).toMatchSnapshot();
+	});
+
+	it('should add multi-part accordion component theme', async () => {
+		const themeFilePath = await getFilePath(`./output/src/styles/theme/index.ts`);
+		const accordionComponentThemeFilePath = await getFilePath(`./output/src/styles/theme/components/accordion.ts`);
+
+		const { findByText, userEvent } = cli;
+
+		// Init theme file first
+		const themeInitializer = await render('plop', [
+			`--plopfile=${resolve(__dirname, './plopfile.js')}`,
+			`--dest=${resolve(__dirname, './output')}`,
+			'theme',
+		]);
+
+		expect(await themeInitializer.findByText('Select a command:')).toBeInTheConsole();
+
+		expect(await themeInitializer.findByText(`❯ ${INIT_COMMAND}`)).toBeInTheConsole();
+
+		themeInitializer.userEvent.keyboard('[Enter]');
+
+		await waitFor(async () => fs.promises.stat(themeFilePath));
+
+		// Add colors foundation
+
+		expect(await findByText('Select a command:')).toBeInTheConsole();
+
+		userEvent.keyboard('[ArrowDown]');
+		userEvent.keyboard('[ArrowDown]');
+
+		expect(await findByText(`❯ ${ADD_COMPONENT_COMMAND}`)).toBeInTheConsole();
+
+		userEvent.keyboard('[Enter]');
+
+		expect(await findByText('Enter component name:')).toBeInTheConsole();
+
+		await userEvent.keyboard('accordion');
+		await userEvent.keyboard('[Enter]');
+
+		expect(await findByText('Select a component type:')).toBeInTheConsole();
+
+		await userEvent.keyboard('[ArrowDown]');
+
+		expect(await findByText(`❯ ${MULTI_PART_COMPONENT}`)).toBeInTheConsole();
+
+		await userEvent.keyboard('[Enter]');
+
+		await waitFor(async () => fs.promises.stat(accordionComponentThemeFilePath));
+
+		const accordionComponentTheme = fs.readFileSync(accordionComponentThemeFilePath, 'utf8');
+
+		expect(accordionComponentTheme).toMatchSnapshot();
+
+		await waitFor(async () => {
+			const theme = fs.readFileSync(themeFilePath, 'utf8');
+
+			expect(theme).toContain(`import { accordionTheme as Accordion } from './components/accordion';`);
+			expect(theme).toContain(`Accordion,`);
+		});
+
+		const theme = fs.readFileSync(themeFilePath, 'utf8');
 		expect(theme).toMatchSnapshot();
 	});
 });
