29c04db9528978612321a2b6ccdbacad46172eb3
Kristian Djaković <kristian.djakovic@infinum.hr>
2023-02-07 17:38:37 +0100

    feat: write more tests

--- packages/use-merge-refs/jest.config.js
+++ packages/use-merge-refs/jest.config.js
@@ -5,31 +5,11 @@
 
 /** @type {import('jest').Config} */
 module.exports = {
-	// All imported modules in your tests should be mocked automatically
-	// automock: false,
-
-	// Stop running tests after `n` failures
-	// bail: 0,
-
-	// The directory where Jest should store its cached dependency information
-	// cacheDirectory: "/private/var/folders/5b/3yjk1s017yq9429wrq06dp400000gn/T/jest_dx",
-
-	// Automatically clear mock calls, instances, contexts and results before every test
 	clearMocks: true,
-
-	// Indicates whether the coverage information should be collected while executing the test
 	collectCoverage: true,
-
-	// An array of glob patterns indicating a set of files for which coverage information should be collected
-	// collectCoverageFrom: undefined,
-
-	// The directory where Jest should output its coverage files
 	coverageDirectory: 'coverage',
 
-	// An array of regexp pattern strings used to skip coverage collection
-	// coveragePathIgnorePatterns: [
-	//   "/node_modules/"
-	// ],
+	coveragePathIgnorePatterns: ['/node_modules/'],
 
 	// Indicates which provider should be used to instrument code for coverage
 	coverageProvider: 'v8',
--- packages/use-merge-refs/tests/useMergeRefs.test.tsx
+++ packages/use-merge-refs/tests/useMergeRefs.test.tsx
@@ -30,4 +30,40 @@ describe('useMergeRefs', () => {
 
 		render(<App />);
 	});
+
+	it('renders with other types', () => {
+		const noop = jest.fn();
+
+		const App = () => {
+			const ref1 = React.useRef<HTMLElement | null>();
+
+			const mergedRefs = useMergeRefs(ref1, noop, null, undefined, {});
+
+			return <div ref={mergedRefs}>app</div>;
+		};
+
+		render(<App />);
+
+		expect(noop).toBeCalled();
+	});
+
+	it('trows if ref is freezed', () => {
+		const freezed = Object.freeze({ current: null });
+
+		const App = () => {
+			const ref1 = React.useRef<HTMLElement | null>();
+
+			const mergedRefs = useMergeRefs(ref1, freezed);
+
+			return <div ref={mergedRefs}>app</div>;
+		};
+
+		try {
+			<App />;
+
+			expect(false).toBe(true);
+		} catch (e) {
+			expect(e).toBeInstanceOf(Error);
+		}
+	});
 });
