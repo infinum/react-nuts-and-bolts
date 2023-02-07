5542cf7d73696948341b83bdadec8a353108725b
Kristian Djaković <kristian.djakovic@infinum.hr>
2023-02-07 22:27:53 +0100

    Create a shared infinum jest config

---
+++ packages/jest/src/jest.ts
@@ -0,0 +1,37 @@
+import type { Config } from 'jest';
+
+export function infinumJest() {
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	return (customJestConfig: any = {}) => {
+		return async (): Promise<Config> => {
+			const resolvedJestConfig =
+				(typeof customJestConfig === 'function' ? await customJestConfig() : customJestConfig) ?? {};
+
+			return {
+				...customJestConfig,
+
+				transform: {
+					// Use SWC to compile tests
+					'^.+\\.(t|j)sx?$': '@swc/jest',
+					...(resolvedJestConfig.transform ?? []),
+				},
+
+				testPathIgnorePatterns: [
+					// Don't look for tests in node_modules
+					'/node_modules/',
+					// Don't look for tests in dist folder
+					'/dist/',
+					// Custom config can append to testPathIgnorePatterns but not modify it
+					...(resolvedJestConfig.testPathIgnorePatterns ?? []),
+				],
+
+				watchPathIgnorePatterns: [
+					// Don't re-run if change happens in node_modules
+					'/node_modules/',
+					// Custom config can append to watchPathIgnorePatterns but not modify it
+					...(resolvedJestConfig.watchPathIgnorePatterns ?? []),
+				],
+			};
+		};
+	};
+}
