3f4aeb14334c3603a88719bd40e604c3d54ccfa3
Kristian Djaković <kristian.djakovic@infinum.hr>
2023-02-08 14:31:40 +0100

    Fix build issues on jest

--- packages/jest/tsup.config.ts
+++ packages/jest/tsup.config.ts
@@ -1,11 +1,5 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-	clean: true,
-	format: ['cjs', 'esm'],
-	splitting: false,
-	outExtension(ctx) {
-		return { js: `.${ctx.format}.js` };
-	},
 	entry: ['src/index.ts', 'src/environment.ts'],
 });
